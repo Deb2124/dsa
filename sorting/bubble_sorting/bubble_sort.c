2ffe204227e184b40c58a09982c33ef704efd46c
Deb2124 <72875364+Deb2124@users.noreply.github.com>
2024-07-07 01:12:22 +0530

    Update bubble_sort.c

--- sorting/bubble_sorting/bubble_sort.c
+++ sorting/bubble_sorting/bubble_sort.c
@@ -7,13 +7,17 @@ void swap(int *a, int *b) {
 }
 //sorting the array
 int* bubble_sort(int n, int arr[]) {
-    int i, j;
+    int i, j, swapped;
     for (i = 0; i < n-1; i++) {
+        swapped = 0;
         for (j = 0; j < n-i-1; j++) {
             if (arr[j] > arr[j+1]) {
                 swap(&arr[j], &arr[j+1]);
+                swapped++;
             }
         }
+        if (swapped == 0)
+            break;
     }
     return arr;
 }
